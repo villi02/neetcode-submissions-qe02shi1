e420bd50b0d73d9e8bfb655628c6e29a77ceb6c5
Vilhjalmur Arnar Vilhjalmsson <79274024+villi02@users.noreply.github.com>
2026-04-07 23:59:01 +0800

    Add: binary-search - submission-11

---
+++ Data Structures & Algorithms/binary-search/submission-11.py
@@ -0,0 +1,13 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums)-1
+
+        while left <= right:
+            mid = (right+left)//2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] > target: # Need to move right down
+                right = mid-1
+            else:
+                left = mid+1
+        return -1

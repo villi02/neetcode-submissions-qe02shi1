2ab3268bde8a25c4ecda0590c06b00d9f712d399
Vilhjalmur Arnar Vilhjalmsson <79274024+villi02@users.noreply.github.com>
2026-05-26 16:07:13 +0800

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.py
@@ -0,0 +1,24 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        # Maybe just have two pointers, of length s1, then keep counts of the variables inside s2 inside the window
+
+        counts = {}
+
+        for s in s1:
+            counts[s] = 1 + counts.get(s, 0)
+        
+        l = 0
+
+        for r in range(len(s2)):
+            print(f"l {l}, r {r}, counts: {counts}, max.counts{max(counts.values())}")
+            if (r-l +1) > len(s1):
+                if s2[l] in counts:
+                    counts[s2[l]] += 1
+                l += 1
+            if s2[r] in counts:
+                counts[s2[r]] -= 1
+            if max(counts.values()) == 0:
+                print(f"counts after finding all zero: {counts}")
+                return True
+        return False
+            

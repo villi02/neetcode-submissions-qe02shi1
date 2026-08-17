968de0e2c7972a8f551a9409dc8c8199d76d57a1
Vilhjalmur Arnar Vilhjalmsson <79274024+villi02@users.noreply.github.com>
2026-08-17 18:32:35 +0200

    Add: copy-linked-list-with-random-pointer - submission-3

---
+++ Data Structures & Algorithms/copy-linked-list-with-random-pointer/submission-3.py
@@ -0,0 +1,58 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+
+    def newNode(self, node):
+        new = Node(node.val)
+        new.next = None
+        new.random = None
+        return new
+
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        
+        if not head:
+            return None
+        
+        explored = {}
+        explored[head] = self.newNode(head)
+
+        newHead = explored[head]
+        iter_head = newHead
+        rand_node = None
+        next_node = None
+
+        while head:
+
+            if head.next:
+                if head.next not in explored:
+                    next_node = self.newNode(head.next)
+                    explored[head.next] = next_node
+                else:
+                    next_node = explored[head.next]
+            else:
+                next_node = None
+            
+
+            if head.random:
+                if head.random not in explored:
+                    rand_node = self.newNode(head.random)
+                    explored[head.random] = rand_node
+                else:
+                    rand_node = explored[head.random]
+            else:
+                rand_node = None
+            
+            iter_head.next = next_node
+            iter_head.random = rand_node
+
+            iter_head = iter_head.next
+            head = head.next
+
+        return newHead

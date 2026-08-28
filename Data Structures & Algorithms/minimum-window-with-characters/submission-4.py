ab44b715f623db52af29e293d83554c06c01463a
Vilhjalmur Arnar Vilhjalmsson <79274024+villi02@users.noreply.github.com>
2026-08-28 15:37:13 +0000

    Add: minimum-window-with-characters - submission-4

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-4.py
@@ -0,0 +1,70 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if len(s) < len(t):
+            return ""
+        
+        """
+        res = [0, len(s)-1]
+
+        tracker = {}
+        # Maybe also keep track of the indexes with a queue
+        queue = deque()
+
+        for char in t:
+            tracker[char] = False
+        
+        l, r = 0, 1
+
+        while r < len(s):
+            
+            if s[r] in tracker: # Now logic for moving if needed
+                if (tracker[s[r]]) and (s[l] == s[r]): # now we can safely move
+                    newl = queue.popleft()
+                    l = newl
+                    
+
+
+        return s[res[0]: res[1]+1]
+        """
+        # Maybe just keep track of counts and when all are found, then simply close shrink the window
+
+        tracker = {}
+        freq = {}
+
+        l, r = 0,0
+        satisfied = {}
+        smallest = len(s)+1
+        res = [l,r]
+
+        for char in t:
+            if char in tracker:
+                freq[char] += 1
+            else:
+                freq[char] = 1
+                tracker[char] = 0
+            
+        
+        need = len(tracker)
+
+        foundWindow = False
+
+        while r < len(s):
+            if s[r] in tracker:
+                tracker[s[r]] += 1
+                if tracker[s[r]] >= freq[s[r]]:
+                    satisfied[s[r]] = True
+            
+            while len(satisfied) == len(freq): # now we shrink the window
+                foundWindow = True
+                d = r - l+1
+                if d < smallest:
+                    res = [l, r]
+                    smallest = d
+                if s[l] in tracker:
+                    tracker[s[l]] -= 1
+                    if tracker[s[l]] < freq[s[l]]:
+                        del satisfied[s[l]]
+                l += 1
+            r += 1
+
+        return s[res[0]: res[1]+1] if foundWindow else ""

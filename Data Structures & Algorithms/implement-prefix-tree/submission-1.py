6ef1165c060f090b8d5d520459b3bf835417f588
Vilhjalmur Arnar Vilhjalmsson <79274024+villi02@users.noreply.github.com>
2026-09-22 14:54:14 +0200

    Add: implement-prefix-tree - submission-1

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-1.py
@@ -0,0 +1,32 @@
+class PrefixTree:
+
+    def __init__(self):
+        self.root = {}
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            node = node.setdefault(char, {})
+        node["#"] = True
+
+
+    def search(self, word: str) -> bool:
+        node = self.root
+
+        for char in word:
+            if char not in node:
+                return False
+            node = node[char]
+        return "#" in node
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+
+        for char in prefix:
+            if char not in node:
+                return False
+            node = node[char]
+        return True
+        
+        
